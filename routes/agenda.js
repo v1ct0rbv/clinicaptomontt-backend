d8a9fa1395b7add98bb5ae880881c0070e9cfe08
victor <victor.bv.996@outlook.com>
2021-04-11 20:38:08 -0400

    'asdasfa'

--- routes/agenda.js
+++ routes/agenda.js
@@ -19,120 +19,120 @@ router.post("/scraping-create",create)
 
 
 
-// async function scrapingAuto (x,y){
+async function scrapingAuto (x,y){
 
-//      // const{user,password,fechaInicio,fechaFin} = req.body
-//      const user = 'hgoringpm'
-//      const password = 'agenda2020.'
-//      const fechaInicio = x
-//      const fechaFin = y
+     // const{user,password,fechaInicio,fechaFin} = req.body
+     const user = 'hgoringpm'
+     const password = 'agenda2020.'
+     const fechaInicio = x
+     const fechaFin = y
      
  
-//      const browser = await puppeteer.launch({
-//          'args' : [
-//            '--no-sandbox',
-//            '--disable-setuid-sandbox'
-//          ]
-//        });
+     const browser = await puppeteer.launch({
+         'args' : [
+           '--no-sandbox',
+           '--disable-setuid-sandbox'
+         ]
+       });
      
-//      const page = await browser.newPage();
-//      //pagina
-//      const pagLogin = `https://backoffice.prod.regionales.procloudhub.com/#/login`;
+     const page = await browser.newPage();
+     //pagina
+     const pagLogin = `https://backoffice.prod.regionales.procloudhub.com/#/login`;
  
  
-//      await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve(__dirname,'../AgendadosCSV')});
+     await page._client.send('Page.setDownloadBehavior', {behavior: 'allow', downloadPath: path.resolve(__dirname,'../AgendadosCSV')});
  
-//      await page.goto(pagLogin, { waitUntil: 'networkidle0' });
+     await page.goto(pagLogin, { waitUntil: 'networkidle0' });
  
-//      //Iniciar Sesión
-//      await page.waitForSelector('input[name=""]');
-//      await page.type('input[name=""]', `${user}`);
-//      await page.type('input[name="pass"]', `${password}`);
-//      await page.click('input[class="loginReception"]'); 
+     //Iniciar Sesión
+     await page.waitForSelector('input[name=""]');
+     await page.type('input[name=""]', `${user}`);
+     await page.type('input[name="pass"]', `${password}`);
+     await page.click('input[class="loginReception"]'); 
      
-//      //Abrir exportacion de citas
-//      await page.waitForSelector('a[class="patient-btn appointment-export"]');
-//      await page.click('a[class="patient-btn appointment-export"]');
+     //Abrir exportacion de citas
+     await page.waitForSelector('a[class="patient-btn appointment-export"]');
+     await page.click('a[class="patient-btn appointment-export"]');
      
-//      //Seleccionar todas las areas
-//      await page.waitForSelector('select[name="areaSelect"]')
-//      await page.click('select#areaSelect')
-//      await page.waitForTimeout(1000);
-//      await page.keyboard.press('ArrowDown');
-//      await page.keyboard.press('Enter');
+     //Seleccionar todas las areas
+     await page.waitForSelector('select[name="areaSelect"]')
+     await page.click('select#areaSelect')
+     await page.waitForTimeout(1000);
+     await page.keyboard.press('ArrowDown');
+     await page.keyboard.press('Enter');
     
-//      //Seleccionar citados
-//      await page.waitForSelector("select#appointmentStatus");
-//      await page.select("select#appointmentStatus", "Bofoked");
+     //Seleccionar citados
+     await page.waitForSelector("select#appointmentStatus");
+     await page.select("select#appointmentStatus", "Bofoked");
      
-//      //Seleccionar fechas
-//      const input = await page.$('input[name="startDateFilter"]');
-//      await input.click({ clickCount: 3 })
-//      await input.type(`${fechaInicio}`);
+     //Seleccionar fechas
+     const input = await page.$('input[name="startDateFilter"]');
+     await input.click({ clickCount: 3 })
+     await input.type(`${fechaInicio}`);
  
-//      const input2 = await page.$('input[name="endDateFilter"]');
-//      await input2.click({ clickCount: 3 })
-//      await input2.type(`${fechaFin}`);
+     const input2 = await page.$('input[name="endDateFilter"]');
+     await input2.click({ clickCount: 3 })
+     await input2.type(`${fechaFin}`);
          
-//      //Descargar Citado
-//      await page.click('button#export-button-popup')
-//      console.log('Descargando Citados...')
+     //Descargar Citado
+     await page.click('button#export-button-popup')
+     console.log('Descargando Citados...')
  
-//      await page.waitForTimeout(15000);
+     await page.waitForTimeout(15000);
      
      
      
-//      console.log('Descarga finalizada')
+     console.log('Descarga finalizada')
  
-//      // set viewport and user agent (just in case for nice viewing)
-//      await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
+     // set viewport and user agent (just in case for nice viewing)
+     await page.setUserAgent('Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/78.0.3904.108 Safari/537.36');
  
   
  
-//      await browser.close();
-//      res.send({
-//          data:'Scraping listo'
-//      })
-// }
+     await browser.close();
+     res.send({
+         data:'Scraping listo'
+     })
+}
 
-// setInterval(
-//     function(){
-//         var date = new Date();
-//         var diaActual = date.getDate()
+setInterval(
+    function(){
+        var date = new Date();
+        var diaActual = date.getDate()
 
-//         var first = date.getDate() - date.getDay() +1; // First day is the day of the month - the day of the week
-//         var last = first + 6; // last day is the first day + 6
+        var first = date.getDate() - date.getDay() +1; // First day is the day of the month - the day of the week
+        var last = first + 6; // last day is the first day + 6
 
-//         var firstday = new Date(date.setDate(first));
-//         var lastday = new Date(date.setDate(last));
+        var firstday = new Date(date.setDate(first));
+        var lastday = new Date(date.setDate(last));
 
-//         var lunes =firstday.getDate()+"-"+("0" + (firstday.getMonth() + 1)).slice(-2)+'-'+firstday.getUTCFullYear()
-//         var domingo = lastday.getDate()+"-"+("0" + (lastday.getMonth() + 1)).slice(-2)+'-'+lastday.getUTCFullYear()
+        var lunes =firstday.getDate()+"-"+("0" + (firstday.getMonth() + 1)).slice(-2)+'-'+firstday.getUTCFullYear()
+        var domingo = lastday.getDate()+"-"+("0" + (lastday.getMonth() + 1)).slice(-2)+'-'+lastday.getUTCFullYear()
 
-//         if(lastday.getDate() < 10){
-//             var dom = '0'+lastday.getDate()
-//             var domingo = dom+domingo.slice(1)
-//         }
+        if(lastday.getDate() < 10){
+            var dom = '0'+lastday.getDate()
+            var domingo = dom+domingo.slice(1)
+        }
 
-//         if(firstday.getDate() < 10){
-//             var lun = '0'+firstday.getDate()
-//             var lunes = lun+lunes.slice(1)
-//         }
+        if(firstday.getDate() < 10){
+            var lun = '0'+firstday.getDate()
+            var lunes = lun+lunes.slice(1)
+        }
 
-//         console.log(
-//             diaActual,lunes,domingo,lastday.getDate(),dom
-//         )
+        console.log(
+            diaActual,lunes,domingo,lastday.getDate(),dom
+        )
 
-//         var hoy = new Date();
-//         // if(hoy.getHours() === 5){
-//         console.log('Es hora del scraping')
-//         scrapingAuto(lunes,domingo)
-//         .then(res =>
-//             console.log(res))
-//         .catch(err => console.log(err))
-//         // }
-//     }
-//     ,1000*60*60)
+        var hoy = new Date();
+        // if(hoy.getHours() === 5){
+        console.log('Es hora del scraping')
+        scrapingAuto(lunes,domingo)
+        .then(res =>
+            console.log(res))
+        .catch(err => console.log(err))
+        // }
+    }
+    ,1000*60*6)
 
 module.exports = router
 
